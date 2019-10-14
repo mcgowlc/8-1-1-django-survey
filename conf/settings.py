5a35271bd8a044bb0ba3770ca3eeae6eec436db5
Linda <mcgowlc@live.com>
2019-10-14 16:02:08 -0400

    updates for deployment

--- conf/settings.py
+++ conf/settings.py
@@ -120,13 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
